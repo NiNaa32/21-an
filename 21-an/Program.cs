57124d2e29dd8ac20005e00f09b601c734cc0eed
NiNaa32 <112417273+NiNaa32@users.noreply.github.com>
2022-09-02 15:20:38 +0200

    pseudokod

---
+++ 21-an/Program.cs
@@ -0,0 +1,39 @@
+﻿// Skriv välkomstmeddelande
+
+// Sätt menyVal till "0"
+
+// while (menyval != ”4”)
+
+// Skriv ut meny
+
+// Läs in menyVal
+
+// switch menyVal
+// case 1:Spela en omgång av 21:an
+        // Spela en omgång av 21:an
+        case "1":
+            // Sätt spelarensPoäng och datornsPoäng till 0
+            // Dra två kort per spelare
+
+            // sätt kortVal till "j"
+            // while (kortVal != "n" och spelarensPoäng <= 21)
+                // Skriv ut poängställningen
+                // Fråga om spelaren vill ha ett till kort
+                // Läs in kortval
+
+                // switch kortVal
+                    // case "j": Dra ett kort till
+                    // case "n": Gör ingenting (loopen kommer sluta köras)
+                    // default: Skriv att alternativet var ogiltigt
+            break;
+        // Gå tillbaka till huvudmenyn om användaren har över 21
+        // Datorn drar kort tills den vinner eller går över 21
+        // Skriv ut datorns och spelarens poäng
+        // Undersök vem som vann
+        // Om spelaren vann så får hen skriva in sitt namn
+
+
+// case 2: Visa senaste vinnaren
+// case 3: Visa spelets regler
+// case 4: Gör ingenting (programmet avslutas)
+// default: Skriv att alternativet var ogiltigt
